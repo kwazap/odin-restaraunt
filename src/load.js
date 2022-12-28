4e6d7670780bc88f0e28f1c37ff74259d956a820
Ivan <kwakiovoono@gmail.com>
2022-12-28 09:31:06 +0100

    Button listeners and switch function call

--- src/load.js
+++ src/load.js
@@ -1,13 +1,19 @@
 import { makeHeader } from "./header"
 import { makeMain } from "./content"
+import { switchContent } from "./switch"
 
 const loadPage = function (params) {
     const container = document.querySelector('.content')
+
     const header = makeHeader()
-    const main = makeMain()
     container.appendChild(header)
+
+    const main = makeMain()
     container.appendChild(main)
 
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', switchContent)
+    })
 }
 
 
