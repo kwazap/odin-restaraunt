3a736b9df7ec65117c9815be152e7e794810d5e5
Ivan <kwakiovoono@gmail.com>
2022-12-28 07:23:18 +0100

    add header skeleton content

--- src/header.js
+++ src/header.js
@@ -1,7 +1,26 @@
 
 const makeHeader = function () {
+
     const header = document.createElement('div')
     header.className = 'header'
+
+    const title = header.appendChild(document.createElement('h1'))
+    title.textContent = 'Cheese Restaurant'
+
+    const navWrapper = header.appendChild(document.createElement('div'))
+    navWrapper.className = 'nav-wrapper'
+
+    const homeButton = navWrapper.appendChild(document.createElement('button'))
+    homeButton.className = 'home-button'
+    homeButton.textContent = 'Home'
+    
+    const menuButton = navWrapper.appendChild(document.createElement('button'))
+    menuButton.className = 'menu-button'
+    menuButton.textContent = 'Menu'
+
+    const contactButton = navWrapper.appendChild(document.createElement('button'))
+    contactButton.className = 'contact-button'
+    contactButton.textContent = 'Contact'
 
     return header
 
