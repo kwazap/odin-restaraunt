a9a3c9a8cebef2dadc10fe2f6fc57fe724ce5740
Ivan <kwakiovoono@gmail.com>
2022-12-28 08:34:18 +0100

    Add credits for background photo

--- src/content.js
+++ src/content.js
@@ -3,6 +3,15 @@ const makeMain = function () {
     const content = document.createElement('div')
     content.className = 'main'
 
+    const credits = document.createElement('div') 
+    credits.className = 'credits'
+    content.appendChild(credits)
+    
+    const creditsLink = document.createElement('a')
+    creditsLink.href = 'https://unsplash.com/@desertroseco'
+    creditsLink.textContent = 'Background by Aleisha Kalina on unsplash'
+    credits.appendChild(creditsLink)    
+
     return content
 }
 
